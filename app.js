d47df2aabc8e1813ce846e068fca75afbe1f8a8f
Mehul Gupta <45738333+mehulrnsit@users.noreply.github.com>
2022-02-15 12:54:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -58,6 +58,11 @@ app.get("/state",async function(req, res) {
 });
 
 
-app.listen(3000, function() {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
