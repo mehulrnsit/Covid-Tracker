1033f54f91fd3b56398b50fda60adf8c64ffc118
Mehul Gupta <45738333+mehulrnsit@users.noreply.github.com>
2022-02-15 12:46:50 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,63 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+const https = require("https");
+const request = require("request");
+
+const app = express();
+
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/",function(req,res){
+  const url = `https://api.covidtracking.com/v1/us/current.json`;
+  
+  https.get(url, function (response) {
+   
+    response.on("data", function (data) {
+      const countryData1 = JSON.parse(data);
+      
+      const i1 = 1;
+      res.render("home",{content: countryData1[0],i:i1});
+    });
+  });
+  
+  
+});
+
+app.get("/states",function(req,res){
+
+  var url="https://api.covidtracking.com/v1/states/info.json";
+  request.get(url,function(error,response,body){
+    states =JSON.parse(body);
+    var I=1;
+    // console.log(states);
+    res.render("states",{contents:states,i:I});
+  });
+    
+ 
+});
+
+app.get("/state",async function(req, res) {
+ 
+  const st =  req.query.st;
+  const Name = req.query.Name;
+ 
+  var url="https://api.covidtracking.com/v1/states/current.json";
+  request.get(url,function(error,response,body){
+    states =JSON.parse(body);
+    var I=1;
+    res.render("state",{contents:states,i:I,st:st,Name:Name});
+  });
+});
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
